bd3215db46ce51d7b6724beac97241d40fccb98c
James Grieve <jgrieve@ualberta.ca>
2022-03-16 10:36:07 -0600

    If / else;

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,25 @@
 async function main() {
     // This is where the code you're actually experimenting with goes.
-    output("JavaScript is fun... Right..? Guys...?");
-
-    //Number
-    let myNumber = 41;
-    //String
-    let myString = "James";
-    //Boolean
-    let myBoolean = true; 
     
-    /* Arithmetic Operators (Math)
-    + (Addition) Sums the two operands.
-    - (Subtraction) Gets the difference of the second operand from the first.
-    * (Multiplication) Multiplies the two operands.
-    / (Division) Gets the quotient of the second operand from the first.
-    % (Modulus) Gets the remainder of division between the first and second operands.
+    /* Decisions
+    Decisions allow you to branch a program's logic flow based on the result of a boolean expression (one that evaluates to a boolean).
+    One logic path will execute if the expression (called a condition) is true, and the other will execute if it is false.
     */
 
-    /* Relational Operators (Comparison)
-    > (Greater Than) Is the first operand larger than the second?
-    >= (Greater Than, or Equal To) Is the first operand larger than or equal to the second?
-    < (Less Than) Is the first operand smaller than the second?
-    <= (Less Than, or Equal To) Is the first operand smaller than or equal to the second?
-    == (Equal To) Is the first operand equal to the second?
-    != (Not Equal To) Is the first operand not equal to the second?
-    === (Identity) Is the first operand equal to the second, without implicit conversions?
-    */
+    if(42<10)
+    // This side runs if the condition is true!
+    {
+        let myNumber = 10;
+        output(myNumber + " is a cool number.");
+    }
+    else 
+    // This side runs if the condition is false!
+    {
+        output("False!");
+    }
 
-    /* Logical Operators 
-    || (Or) Returns true if AT LEAST ONE of the operands is true.
-    && (And) Returns true if BOTH operands are true.
-    ! (Not) Returns the opposite of the SINGLE operand.
-    ^ (Xor) Returns true if EXACTLY ONE operand is true.
-    */
-
-    // An expression is a set of variables, values and/or operators that evaluates to a single value. 
-    // If a statement is a sentence and a code block is a paragraph, you can think of expressions similar to clauses (IE parts of sentences).
-
-    /* Expression Evaluation Example
-    let myInt = 42;
-    let myDecimal = 3.14;
-    myInt >= 40 || ((myDecimal < 3) || false)
-
-    myInt >= 40 || ((false) || false)
-    myInt >= 40 || (false)
-    true || (false)
-    true
-    */
+    // Everything before or after the decision runs either way.
+    output("Either way!");
 }
 
 
