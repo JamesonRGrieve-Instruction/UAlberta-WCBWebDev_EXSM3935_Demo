9efd3be6aac7acfc3fee9d8e88bc2bbe6e2361b0
James Grieve <jgrieve@ualberta.ca>
2022-04-06 11:39:25 -0600

    Exercise 02 material;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -23,9 +23,51 @@ async function main() {
 
     // Concatenation: Gluing two strings together.
     output("Hello, "+name+"!");
+
     // Template Literal: A string with placeholder slots that get variables added.
     output(`Hello, ${name}!`);
 
+    /*
+    Operators allow multiple literals and variables to be combined to form complex expressions.
+    Arithmetic Operators: + - * / % (addition, subtraction, multiplication, division, modulus).
+    Relational Operators: > < >= <= == != === (greater, less, greater-equal, less-equal, equal, not-equal, identity).
+    Logical Operators: ! || && ^ (not, or, and, xor).
+
+    Operators Execute In This Order:
+    Parentheses (including function parentheses)
+    Logical Not
+    Arithmetic
+    Relational
+    Logical (Other Than Not)
+    */
+
+    let userNumber = await input("Please enter a number: ");
+    if(userNumber > 10)
+    {
+        output("That's bigger than 10!");
+    }
+    else if (userNumber == 10)
+    {
+        output("That's 10!");
+    }
+    else
+    {
+        output("That's smaller than 10!");
+    }
+
+    if(name == "Awesome" && userNumber == 10)
+    {
+        output("That's the magic combination! Good job!");
+    }
+
+    if (name != "Test")
+    {
+        output("Not test.");
+    }
+    if (!(name == "Test"))
+    {
+        output("Not test.");
+    }
 }
 
 
