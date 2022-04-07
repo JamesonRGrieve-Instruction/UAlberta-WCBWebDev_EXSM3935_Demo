2f772418a0abeb0a7c4966973ee07cfdf827a3ee
James Grieve <jgrieve@ualberta.ca>
2022-04-07 09:23:57 -0600

    Remove template code;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,6 @@
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
 
-    while (name != "Exit") 
-    {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
-    }
+    
 }
 
 
