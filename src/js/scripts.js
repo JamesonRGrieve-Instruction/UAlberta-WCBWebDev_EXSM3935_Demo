0cef76dcc931667942a3ce25a69bcc1708556572
James Grieve <jgrieve@ualberta.ca>
2022-04-13 09:14:51 -0600

    Basic function with no returns and no parameters;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,13 @@
 async function main() {
-    
+    // Functions allow us to wrap multiple statements into a single name, and call them easier.
+    printMenu();
 }
 
-
+// When printMenu() is called on line 3, it jumps into here and starts executing. When it's done, it returns to line 8 and continues.
+function printMenu() {
+    output("---Menu---");
+    output("1. Do Stuff");
+    output("2. Do More Stuff");
+    output("3. Do Extra Stuff");
+    output("0. Exit");
+}
