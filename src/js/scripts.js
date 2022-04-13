f38080ce1d236d6266e6ab24798f80bf41a9a848
James Grieve <jgrieve@ualberta.ca>
2022-04-13 11:23:18 -0600

    q01 solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,49 +1,34 @@
 async function main() {
-    // Functions allow us to wrap multiple statements into a single name, and call them easier.
-    printMenu();
+    /*
+        q01: Write a function that takes in a number. Return true if the number is even, and false if it is odd. 0 is even, for this purpose. IE:
+        isEven(1) → false
+        isEven(12) → true
+        isEven(42585) → false
+    */
+   output(isEven(5));
+}
+function isEven(number) {
+    let toReturn;
 
-    // Lines: 3, 12, 13, 14, 15, 16, then returns to 4 and ends.
+    if (number % 2 == 0) {
+        toReturn = true;
+    }
+    else {
+        toReturn = false;
+    }
 
-    greetUser("James");
-    greetUser("John");
-    greetUser("Jane");
-
-    output(giveMePi());
-
-    output(firstLetterAndLength("James"));
-    output(firstLetterAndLength("Antidisestablishmentarianism"));
+    return toReturn;
 }
 
-// When printMenu() is called on line 3, it jumps into here and starts executing. When it's done, it returns to line 8 and continues.
-function printMenu() {
-    output("---Menu---");
-    output("1. Do Stuff");
-    output("2. Do More Stuff");
-    output("3. Do Extra Stuff");
-    output("0. Exit");
+function isEvenUnstructured(number) {
+    if (number % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
-function greetUser(name)
-{
-    output("Hello, welcome to the program, "+name);
-    output("I hope you're having a good day.");
-}
 
-// When a function with a return ends, the value returned takes the place of the function call (IE output(giveMePie()) becomes output(3.141592654)).
-function giveMePi()
-{
-    return 3.141592654;
-}
-
-function firstLetterAndLength(string)
-{
-    let toReturn = [];
-    let firstLetter = string[0];
-    let length = string.length;
-    toReturn.push(firstLetter);
-    toReturn.push(length);
-    return toReturn; 
-
-    // One line for those curious.
-    // return [string[0], string.length];
-}
