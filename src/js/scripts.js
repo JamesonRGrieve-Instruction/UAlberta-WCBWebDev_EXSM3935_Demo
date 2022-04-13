604eb688328010676c78dafe7a8a8c02e9f79691
James Grieve <jgrieve@ualberta.ca>
2022-04-13 09:30:15 -0600

    Functions with parameters and returns;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,17 @@
 async function main() {
     // Functions allow us to wrap multiple statements into a single name, and call them easier.
     printMenu();
+
+    // Lines: 3, 12, 13, 14, 15, 16, then returns to 4 and ends.
+
+    greetUser("James");
+    greetUser("John");
+    greetUser("Jane");
+
+    output(giveMePi());
+
+    output(firstLetterAndLength("James"));
+    output(firstLetterAndLength("Antidisestablishmentarianism"));
 }
 
 // When printMenu() is called on line 3, it jumps into here and starts executing. When it's done, it returns to line 8 and continues.
@@ -10,4 +21,29 @@ function printMenu() {
     output("2. Do More Stuff");
     output("3. Do Extra Stuff");
     output("0. Exit");
+}
+
+function greetUser(name)
+{
+    output("Hello, welcome to the program, "+name);
+    output("I hope you're having a good day.");
+}
+
+// When a function with a return ends, the value returned takes the place of the function call (IE output(giveMePie()) becomes output(3.141592654)).
+function giveMePi()
+{
+    return 3.141592654;
+}
+
+function firstLetterAndLength(string)
+{
+    let toReturn = [];
+    let firstLetter = string[0];
+    let length = string.length;
+    toReturn.push(firstLetter);
+    toReturn.push(length);
+    return toReturn; 
+
+    // One line for those curious.
+    // return [string[0], string.length];
 }
